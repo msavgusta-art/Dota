27d51ea23526a71ecf8c9e9454afc59475befeab
msavgusta-art <msavgusta@gmail.com>
2025-12-03 18:36:12 +0500

    2 lesson

--- backend/Users/views.py
+++ backend/Users/views.py
@@ -1,7 +1,1 @@
-from rest_framework import viewsets 
-from .models import Users
-from .serializers import UsersSerializer
 
-class UsersViewSet(viewsets.ModelViewSet):
-    queryset = Users.objects.all()
-    serializer_class = UsersSerializer
